7bd75fdaf0a29e057e6ece2f27b1461b18b5816f
Ethan Ramos <ethan.n.ramos@gmail.com>
2025-03-28 16:20:04 -0400

    Coded the function to display the hangman.

--- completed/intermediate/hangman/hangman.py
+++ completed/intermediate/hangman/hangman.py
@@ -1,7 +1,83 @@
-import random 
+import random
 words = ["coder", "javascript", "java", "rust", "golang"]
 
+
 def pick_word(words):
-  return random.choice(words)
+    return random.choice(words)
+
+
+def display_hangman(tries):
+    stages = [  # final state: head, torso, both arms and legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                 """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                 """,
+                # head, torso and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                 """,
+                # head, torso and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                 """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                 """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                 """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |    
+                   |      
+                   |      
+                   |     
+                   -
+                 """
+    ]
+    print(stages[tries])
 
 
