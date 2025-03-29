b18575711695f885d23e9b66994f2e312165d034
Ethan Ramos <ethan.n.ramos@gmail.com>
2025-03-29 10:25:13 -0400

    Added a helper function to update the guessed word  so far. coded part of the game loop in play()

--- completed/intermediate/hangman/hangman.py
+++ completed/intermediate/hangman/hangman.py
@@ -85,9 +85,9 @@ def display_game(tries, word, prior_letters):
         prior += f"{char}, "
     if len(prior_letters) == 0:
         prior += "None"
+    print(stages[tries])
     print(w)
     print(prior)
-    print(stages[tries])
 
 
 def check_guess_letter(word, guess):
@@ -121,10 +121,36 @@ def get_user_input(word, prior_letters):
             if guess in prior_letters:
                 print("You guessed this letter already")
                 continue
-        return guess, check_word
+        return guess, check_word == "y"
+
+
+def update_guess(working_word, guess, positions):
+    for idx in positions:
+        working_word[idx] = guess
+    return working_word
 
 
 def play():
     words = ["coder", "javascript", "java", "rust", "golang"]
 
     word = pick_word(words)
+    working_word = ["_"] * len(word)
+    prior_letters = []
+    print("Welcome to hangman. You will have 6 guesses to guess the right word")
+    tries = 6
+    word_guessed = False
+    while tries > 0 and not word_guessed:
+        print("\n" + "="*50)
+        display_game(tries, working_word, prior_letters)
+        guess, check_word = get_user_input(word, prior_letters)
+        if check_word:
+            result = check_guess_word(word, guess)
+            if result:
+                word_guessed = True
+            else:
+                tries -= 1
+        else:
+            positions = check_guess_letter(word, guess)
+            if not positions:
+                prior_letters.append(guess)
+                tries -= 1
