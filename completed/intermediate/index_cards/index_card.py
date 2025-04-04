bf9ffb0ef34226ceab6fd677062071491303567c
Ethan Ramos <ethan.n.ramos@gmail.com>
2025-04-04 16:13:18 -0400

    Added function to flip between the name and value of an index card

--- completed/intermediate/index_cards/index_card.py
+++ completed/intermediate/index_cards/index_card.py
@@ -1,5 +1,14 @@
 
 class Index_Card:
     def __init__(self, name, value):
-      self.name = name
-      self.value = value
+        self.name = name
+        self.value = value
+
+    def print_index_card(self):
+        while True:
+            print(f"{self.value}")
+            input("")
+            print(f"{self.name}")
+            if input("Flip card again? (y/n) ").lower() != "y":
+                break 
+    
