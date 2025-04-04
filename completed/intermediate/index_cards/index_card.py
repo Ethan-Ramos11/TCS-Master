802c9fae100959c3257cff2244265fc07ddf167c
Ethan Ramos <ethan.n.ramos@gmail.com>
2025-04-04 16:28:05 -0400

    created compiler and way to add cards to a card set

--- completed/intermediate/index_cards/index_card.py
+++ completed/intermediate/index_cards/index_card.py
@@ -1,4 +1,3 @@
-
 class Index_Card:
     def __init__(self, name, value):
         self.name = name
@@ -10,8 +9,8 @@ class Index_Card:
             input("")
             print(f"{self.name}")
             if input("Flip card again? (y/n) ").lower() != "y":
-                break 
-    
+                break
+
     def update_name(self, new_name):
         if self.name == new_name:
             print("Please enter a new name for the flash card")
@@ -19,11 +18,26 @@ class Index_Card:
         self.name = new_name
         print(f"Name successfully updated to {new_name}")
         return True
-    
+
     def update_value(self, new_value):
         if self.value == new_value:
             print("Please enter a new value for the flash card")
             return False
         self.value = new_value
         print(f"value successfully updated to {new_value}")
-        return True 
+        return True
+
+
+class index_card_set:
+    def __init__(self, name):
+        self.set_name = name
+        self.cards = set()
+
+    def add_card(self, name, value):
+        new_card = Index_Card(name, value)
+        if not new_card in self.cards:
+            self.cards.add(new_card)
+            print(f"Successfully added {name}")
+            return True
+        print("Card is already in the set")
+        return False
