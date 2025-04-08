04336a6b245dab74295d32ed1469d488843501f8
Ethan Ramos <ethan.n.ramos@gmail.com>
2025-04-07 20:23:19 -0400

    Created a function to check if the number of characters meet the number of modifiers

--- completed/beginner/password_generator/password.py
+++ completed/beginner/password_generator/password.py
@@ -1,4 +1,17 @@
 import random
+
+
+def enough_chars(num_chars, modifiers):
+    mods = 0
+    for val in modifiers.values():
+        if val == "y":
+            mods += 1
+
+    if num_chars >= (mods + 1):
+        return True
+    return False
+
+
 def get_input(modifier):
     while True:
         ans = input(
