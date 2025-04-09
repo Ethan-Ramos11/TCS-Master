ccfef71a7094002270f781e955d8116445e461ae
Ethan Ramos <ethan.n.ramos@gmail.com>
2025-04-09 15:14:19 -0400

    Added DOM elements and event listeners

--- completed/advanced/pattern_matching/main.js
+++ completed/advanced/pattern_matching/main.js
@@ -6,3 +6,13 @@ let moves = 0;
 let gameActive = false;
 let timer = 0;
 let timeInterval;
+
+const gameBoard = document.getElementById("game-board");
+const startButton = document.getElementById("start-button");
+const movesDisplay = document.getElementById("moves");
+const matchesDisplay = document.getElementById("matches");
+const timerDisplay = document.getElementById("timer");
+const messageDisplay = document.getElementById("message");
+
+startButton.addEventListener("click", startGame);
+
