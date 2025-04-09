3759dd0ace13223cf0d719d63f45a64ff0b7e16b
Ethan Ramos <ethan.n.ramos@gmail.com>
2025-04-09 15:08:34 -0400

    Added variables to the start of main.js

--- completed/advanced/pattern_matching/main.js
+++ completed/advanced/pattern_matching/main.js
@@ -1,1 +1,8 @@
-
+const SYMBOLS = ["★", "♦", "♠", "♣", "♥", "●", "◆", "▲"];
+let cards = [];
+let flippedCards = [];
+let matchedPairs = 0;
+let moves = 0;
+let gameActive = false;
+let timer = 0;
+let timeInterval;
