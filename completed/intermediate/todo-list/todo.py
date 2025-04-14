aa03170df1fba6af9cb01f235230c7553e2db6d3
Ethan Ramos <ethan.n.ramos@gmail.com>
2025-04-13 21:18:40 -0400

    Added the functions to update the priority and due date

--- completed/intermediate/todo-list/todo.py
+++ completed/intermediate/todo-list/todo.py
@@ -12,5 +12,17 @@ class Task:
 
     def mark_completed(self):
         self.complete = True
-    
-    
+
+    def update_priority(self, new_priority):
+        if self.priority == new_priority:
+            print("Please enter a different priority")
+            return False
+        self.priority = new_priority
+        return True
+
+    def update_due_date(self, new_date):
+        if self.due_date == new_date:
+            print("Please enter a different date")
+            return False
+        self.priority = new_date
+        return True
