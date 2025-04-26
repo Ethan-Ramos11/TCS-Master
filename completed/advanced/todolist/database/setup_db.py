da287984450ef806f4c561ed72fec7939ead3cfd
Ethan Ramos <ethan.n.ramos@gmail.com>
2025-04-26 10:46:32 -0400

    Created the validate conncetion funcion, create_tables, and format_foreign_keys function

--- completed/advanced/todolist/database/setup_db.py
+++ completed/advanced/todolist/database/setup_db.py
@@ -15,3 +15,35 @@ def convert_to_sql_columns(info):
     return s[:-1]
 
 
+def format_foreign_keys(info):
+    s = ""
+    for key, val in info["foreign_keys"]:
+        s += f"FOREIGN KEY ({key}) REFERENCES {val},"
+    return s[:-1]
+
+
+def create_tables(conn, cursor):
+    for table, info in SCHEMA.items():
+        columns = convert_to_sql_columns(info["columns"])
+        foreign_keys = ""
+        if "foreign_keys" in info:
+            foreign_keys = format_foreign_keys(info)
+        query = f"""
+        CREATE TABLE IF NOT EXISTS {table}
+        ({columns}{foreign_keys})
+        """
+        try:
+            cursor.execute(query)
+            conn.commit()
+        except Exception as e:
+            print(f"Exception: {e}")
+
+
+def validate_connection(conn):
+    if conn == None:
+        return False
+    try:
+        conn.execute("SELECT 1")
+        return True
+    except sqlite3.Error:
+        return False
