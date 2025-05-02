ded46ba8cfbd4b3c314f8e637cd36d700df788c2
Ethan Ramos <ethan.n.ramos@gmail.com>
2025-05-02 18:51:44 -0400

    Added preliminary functions in math_quiz function

---
+++ completed/beginner/math-quiz/math_quiz.py
@@ -0,0 +1,21 @@
+import random
+
+
+def pick_numbers():
+    pass
+
+
+def pick_operation():
+    pass
+
+
+def create_question():
+    pass
+
+
+def check_answer(user_answer, correct_answer):
+    pass
+
+
+def get_user_input(question):
+    pass
