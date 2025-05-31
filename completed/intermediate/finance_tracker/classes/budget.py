d6984bbb45e80694a3bd860952ee0cdde0980e76
Ethan Ramos <ethan.n.ramos@gmail.com>
2025-05-31 10:26:45 -0400

    Coded the constructor for the transactions class

---
+++ completed/intermediate/finance_tracker/classes/budget.py
@@ -0,0 +1,9 @@
+
+class Budget:
+    def __init__(self, name: str, max_amount: int, curr_amount: int, time_frame: str):
+        self.name = name
+        self.max_amount = max_amount
+        self.curr_amount = curr_amount
+        self.time_frame = time_frame
+
+    
---
+++ completed/intermediate/finance_tracker/classes/transactions.py
@@ -0,0 +1,6 @@
+
+class Transactions:
+    def __init__(self, name: str, date: str, amount: int):
+        self.name = name
+        self.date = date
+        self.amount = amount
