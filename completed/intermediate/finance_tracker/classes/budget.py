5246be17d0f991ada7cf26d3181682f01ff0068d
Ethan Ramos <ethan.n.ramos@gmail.com>
2025-05-31 10:35:34 -0400

    Coded the add and remove transaction functions

--- completed/intermediate/finance_tracker/classes/budget.py
+++ completed/intermediate/finance_tracker/classes/budget.py
@@ -1,3 +1,5 @@
+from .transactions import Transactions
+
 
 class Budget:
     def __init__(self, name: str, max_amount: int, curr_amount: int, time_frame: str):
@@ -5,5 +7,18 @@ class Budget:
         self.max_amount = max_amount
         self.curr_amount = curr_amount
         self.time_frame = time_frame
+        self.transactions = []
 
+    def add_transaction(self, transaction: Transactions):
+        self.transactions.append(transaction)
+        self.current_amount -= transaction.amount
+
+    def remove_transaction(self, transaction: Transactions) -> bool:
+        if transaction in self.transactions:
+            self.transactions.remove(transaction)
+            self.curr_amount += transaction.amount
+            return True
+        else:
+            return False
+        
     
