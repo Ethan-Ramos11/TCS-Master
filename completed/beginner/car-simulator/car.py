7797afae2f5a7b0fa00b254a2c3ad265e4a070f5
Ethan Ramos <ethan.n.ramos@gmail.com>
2025-06-07 09:56:22 -0400

    Finished car class

--- completed/beginner/car-simulator/car.py
+++ completed/beginner/car-simulator/car.py
@@ -28,3 +28,28 @@ class Car:
     def slow_down(self, amount):
         self.speed = max(0, self.speed - amount)
         print(f"Slowing down by {amount} mph. Speed: {self.speed} mph")
+
+    def brake_fully(self):
+        self.speed = 0
+        print("Full stop. speed: 0mph")
+
+    def reverse(self):
+        if self.gas_left > 0:
+            self.speed = -random.randint(3, 8)
+            self.gas_left -= 1
+            self.distance_traveled += abs(self.speed) * .05
+        else:
+            print("Out of gas")
+
+    def gas_up(self, amount):
+        old_gas = self.gas_left
+        self.gas_left = min(self.max_gas, self.gas_left + amount)
+        amount_filled = self.gas_left - old_gas
+        print(
+            f"Added {amount_filled} gallons of gas. Gas left: {self.gas_left}/{self.max_gas}")
+
+    def display_stats(self):
+        print(f"\n{self.year} {self.make} {self.model}")
+        print(f"   Speed: {self.speed} mph")
+        print(f"   Gas: {self.gas_left}/{self.max_gas} gallons")
+        print(f"   Distance: {self.distance_traveled:.2f} miles")
